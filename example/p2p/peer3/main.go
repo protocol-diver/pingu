6fec7d08f3de79e54195298a3c6a82d20d35b0a4
dbadoy4874 <dbadoy4874@gmail.com>
2022-09-03 16:29:22 +0900

    example: replace `temp` to `example`

---
+++ example/p2p/peer3/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"pingu"
+	"time"
+)
+
+func main() {
+	client, _ := net.ListenUDP("udp", &net.UDPAddr{IP: net.IP{127, 0, 0, 1}, Port: 8755})
+	p := pingu.NewPingu(client, pingu.Config{})
+	p.Start()
+
+	// peer1
+	p.Register("127.0.0.1:8753")
+	// peer2
+	p.Register("127.0.0.1:8754")
+	ticker := time.NewTicker(5 * time.Second)
+	_ = p.BroadcastPingWithTicker(*ticker, 5*time.Second)
+
+	for {
+		time.Sleep(5 * time.Second)
+		fmt.Println(p.PingTable())
+	}
+}
