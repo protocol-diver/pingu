738388e35bb4ac57497b59f7e828bd6f816cebf4
dbadoy4874 <dbadoy4874@gmail.com>
2022-10-15 02:34:54 +0900

    cmd: generate `ping` to `cmd`

---
+++ cmd/ping/ping.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/dbadoy/pingu"
+)
+
+// go run ping.go --timeout 1000 127.0.0.1:1111
+func main() {
+	timeoutFlag := flag.Int("timeout", 1000, "milliseconds")
+	flag.Parse()
+
+	dest := (flag.Arg(0))
+	timeout := time.Duration(*timeoutFlag * 1000000)
+
+	p, err := pingu.NewPingu("127.0.0.1:3821", nil)
+	if err != nil {
+		return
+	}
+
+	p.Start()
+	defer p.Close()
+
+	err = p.PingPongWithRawAddr(dest, timeout)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("ping-pong success ip:", dest, "timeout", timeout)
+}
